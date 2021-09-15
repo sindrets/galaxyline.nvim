5365957e53e29de4410f01b50cd1fef2b811c8e5
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-09-15 08:28:49 -0400

    chore: format Lua files

--- plugin/galaxyline.lua
+++ plugin/galaxyline.lua
@@ -1,9 +1,11 @@
 if vim.fn.has("nvim-0.5") ~= 1 then
-    error("Sorry this plugin only works with versions of Neovim that supports Lua")
-    return
+  error("Sorry this plugin only works with versions of Neovim that supports Lua")
+  return
 end
 
-if vim.g.loaded_galaxyline then return end
+if vim.g.loaded_galaxyline then
+  return
+end
 
 vim.g.loaded_galaxyline = 1
 
