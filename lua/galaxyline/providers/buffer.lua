3c6a118d4ff93755f27ff36dd1f70d0884ef3d4f
Joel Palmer <joelpalmer@users.noreply.github.com>
2021-09-21 06:24:51 -0500

    fix(providers): return the `bufnr` instead of the buffer index (#8)
    Return the actual `bufnr` as opposed to the index of listed buffers. Now you will get the buffer number as seen when running `:ls`.

--- lua/galaxyline/providers/buffer.lua
+++ lua/galaxyline/providers/buffer.lua
@@ -23,18 +23,7 @@ end
 
 -- get buffer number
 buffer.get_buffer_number = function()
-  local buffers = {}
-  for _, val in ipairs(vim.fn.range(1, vim.fn.bufnr("$"))) do
-    if vim.fn.bufexists(val) == 1 and vim.fn.buflisted(val) == 1 then
-      table.insert(buffers, val)
-    end
-  end
-
-  for idx, nr in ipairs(buffers) do
-    if nr == vim.fn.bufnr("") then
-      return idx
-    end
-  end
+  return vim.api.nvim_win_get_buf(0)
 end
 
 return buffer
